a52ea0b5104f99aaf1b180a711c3531fa79bcd2c
robotcdf <cdaofang@gmail.com>
2017-04-24 17:29:05 +0800

    some update

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 git add $(git ls-files -o)
 git add $(git ls-files -m)
-git rm --ignore-unmatch $(git ls-files --deleted)
+git rm $(git ls-files --deleted) --ignore-unmatch 
 git commit -m 'some update'
 git push origin master
