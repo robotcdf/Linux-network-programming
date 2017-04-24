6b4c393be2d4d804591fa8fe06f8dffe7a4190fe
robotcdf <cdaofang@gmail.com>
2017-04-24 17:20:37 +0800

    some update

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-git add 'git ls-files -o'
+git add $(git ls-files -o)
 git add $(git ls-files -m)
-git rm 'git ls-files -d'
+git rm  $(git ls-files -d)
 git commit -m 'some update'
 git push origin master
-
-
