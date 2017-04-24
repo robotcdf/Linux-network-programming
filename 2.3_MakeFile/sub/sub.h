6970e36995eefadb5a27919d046ce418fb5549e6
robotcdf <cdaofang@gmail.com>
2017-04-24 10:31:03 +0800

    update

--- 2.3_MakeFile/sub/sub.h
+++
@@ -1,8 +0,0 @@
-#ifdef _SUB_H_
-#define _SUB_H_
-extern float sub_float(float a,float b);
-extern int sub_int(int a,int b);
-#endif
-
-
-
--- 2.3_MakeFile/sub/sub_int.c
+++
@@ -1,7 +0,0 @@
-int sub_int(int a,int b)
-{
-	return a-b;
-}
-
-
-
