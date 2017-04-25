4f68cd6e2a275e95de57d234346b612b2f006f6a
robotcdf <cdaofang@gmail.com>
2017-04-25 11:02:30 +0800

    some update

--- 04/4.2.4/test.c
+++ 04/4.2.4/test.c
@@ -2,9 +2,10 @@
 #include<unistd.h>
 int main(void)
 {
-	char *args[]={"/bin/ls",NULL};
+	char *args[]={"/bin/echo", "Hello execve!"};
 	printf("系统分配的进程号是：%d\n",getpid());
-	if(execve("/bin/ls",args,NULL)<0)
+//	if(execve("/bin/ls",args,NULL)<0)
+	if(execve("/bin/echo", args,NULL)<0)
 		printf("创建进程出错！\n");
 		
 	return 0;
