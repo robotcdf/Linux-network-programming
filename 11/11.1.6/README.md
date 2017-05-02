e74281d21d84cb02829b0bf615dddb784391840a
robotcdf <cdaofang@gmail.com>
2017-05-02 20:33:58 +0800

    $message

---
+++ 11/11.1.6/README.md
@@ -0,0 +1,9 @@
+1、先编译
+``` bash
+$gcc -o a a.c
+$gcc -o b b.c
+```
+2、运行程序  
+``` bash
+$./b test
+```
--- update.sh
+++ update.sh
@@ -4,5 +4,8 @@ git pull origin master
 git add $(git ls-files ~/idao/Linux-network-programming -o)
 git add $(git ls-files ~/idao/Linux-network-programming -m)
 git rm  --ignore-unmatch [--] $(git ls-files ~/idao/Linux-network-programming -d)
-git commit -m 'some update'
+#git commit -m 'some update'
+echo "Please input commit message:"
+read message
+git commit -m '$message'
 git push origin master
