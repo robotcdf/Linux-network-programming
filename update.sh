bb6de3f507bdef19cb9a6a46eb041a4c3791ae65
robotcdf <cdaofang@gmail.com>
2017-05-02 10:06:01 +0800

    some update

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 git pull origin master
-git add $(git ls-files -o)
-git add $(git ls-files -m)
-git rm  --ignore-unmatch [--] $(git ls-files -d)
+git add $(git ls-files ~/idao/Linux-network-programming -o)
+git add $(git ls-files ~/idao/Linux-network-programming -m)
+git rm  --ignore-unmatch [--] $(git ls-files ~/idao/Linux-network-programming -d)
 git commit -m 'some update'
 git push origin master
