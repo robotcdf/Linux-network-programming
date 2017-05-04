2a26dc0513ac888367355ffa8cad41a220a3ab17
robotcdf <cdaofang@gmail.com>
2017-05-04 09:47:09 +0800

    add commit input and read

--- update.sh
+++ update.sh
@@ -1,8 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+CUR_DIR=$(pwd)
+cd /home/robot/idao/Linux-network-programming
 
 git pull origin master
-git add $(git ls-files ~/idao/Linux-network-programming -o)
-git add $(git ls-files ~/idao/Linux-network-programming -m)
-git rm  --ignore-unmatch [--] $(git ls-files ~/idao/Linux-network-programming -d)
-git commit -m 'some update'
+git add $(git ls-files -o)
+git add $(git ls-files -m)
+git rm  --ignore-unmatch [--] $(git ls-files -d)
+
+echo "git status"
+git status
+
+echo "Please enter the commit message:"    
+read COMMIT_MESSAGE
+if [ "$COMMIT_MESSAGE" == "" ]; then
+    export COMMIT_MESSAGE = "some update"
+fi
+
+git commit -m "${COMMIT_MESSAGE}"
 git push origin master
+
+cd $CUR_DIR
